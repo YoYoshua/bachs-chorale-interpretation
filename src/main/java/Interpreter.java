48a3fc33da8d94caaced57e0c8770fec18c31dee
Marcin Prystupa <32766539+YoYoshua@users.noreply.github.com>
2018-04-15 10:34:48 +0200

    Add Interpreter

--- src/main/java/Interpreter.java
+++ src/main/java/Interpreter.java
@@ -1,6 +1,57 @@
 package main.java;
 
-public class Interpreter {
+import java.util.*;
 
-    public List<List<int>> interpretData()
+public class Interpreter {
+    private Map<String, Float> classes = new HashMap<>();
+    private Map<String, Float> knownParameters = new HashMap<>();
+    private Scanner scanner = new Scanner(System.in);
+
+    public List<List<Float>> interpretData(List<List<String>> stringsList) {
+        List<List<Float>> resultList = new ArrayList<>();
+
+        for (List<String> strings: stringsList) {
+            List<Float> tempList = new ArrayList<>();
+            Float tempFloat = 0.0f;
+
+            for (String string: strings) {
+                try {
+                    tempFloat.parseFloat(string);
+                }
+                catch(NumberFormatException e) {
+                    if(string.equals("YES")) tempFloat = 1.0f;
+                    else if(string.equals("NO")) tempFloat = 0.0f;
+                    else {
+                        tempFloat = insertCustomValue(string);
+                    }
+                }
+                tempList.add(tempFloat);
+            }
+            resultList.add(tempList);
+        }
+        return resultList;
+    }
+
+    private Float insertCustomValue (String string) {
+        Float tempFloat = 0.0f;
+
+        if(knownParameters.containsKey(string)) {
+            return knownParameters.get(string);
+        }
+        else {
+            while(true) {
+                System.out.println("Napotkano nieznany parametr: " + string + ". Jaka ma być jego wartość?");
+                try {
+                    tempFloat = scanner.nextFloat();
+                    knownParameters.put(string, tempFloat);
+                    break;
+                }
+                catch(InputMismatchException e) {
+                    System.out.println("Wprowadzono niewłaściwy parametr!");
+                    scanner.next();
+                }
+            }
+            return tempFloat;
+        }
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,12 +10,15 @@ public class Main {
         File dataFile = new File("resources/jsbach_chorals_harmony.data");
         FileHandler fileHandler = new FileHandler();
         Parser parser = new Parser();
+        Interpreter interpreter = new Interpreter();
 
         List<String> dataList;
         List<List<String>> parsedList;
+        List<List<Float>> interpretedList;
 
         dataList = fileHandler.readFile(dataFile);
         parsedList = parser.parseInput(dataList);
-        System.out.println(parsedList);
+        interpretedList = interpreter.interpretData(parsedList);
+        System.out.println(interpretedList);
     }
 }
